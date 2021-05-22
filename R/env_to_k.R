2bff32b0707c2f5e53964483e05a30bc2cf46c00
Jpomz <jfpomeranz@gmail.com>
2021-05-22 13:17:01 -0600

    minor change to env_to_k() details roxygen

--- R/env_to_k.R
+++ R/env_to_k.R
@@ -3,7 +3,7 @@
 #' @param env Vector of environmental values.
 #' @param k_base Mean carrying capacity K.
 #'
-#' @details This function adds variation to the carrying capacity based on the environment value. When the variation in the `env` variable is high, the carrying capacity varies between 0.5 and 1.5 * `k_base`. When variation in `env` is very low, the observed k is approximately = `k_base`.
+#' @details This function adds variation to the carrying capacity based on the environment value. When the variation in the `env` variable is high, the carrying capacity varies between 0.5 and 1.5 * `k_base`, with most of the density being concentrated at the extremes. When variation in `env` is very low, the observed k is approximately = `k_base`.
 #'
 #'  This function is designed to take the `df_patch$environment` output from the `mcbrnet::brnet()`  as the `env` argument. See the `brnet()` function to control the variation of `environment` in head waters as well as longitudinal variation of environmental values between patches.
 #'
