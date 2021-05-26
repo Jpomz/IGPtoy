66234baf8a97c0a8a0b9a5f0eafd79a127b0a45e
Jpomz <jfpomeranz@gmail.com>
2021-05-26 15:13:16 -0600

    fix name of output column from "basal_k" to "obs_basal_k"

--- R/igp_sim.R
+++ R/igp_sim.R
@@ -356,7 +356,7 @@ igp_sim <- function(n_patch = 20,
                          "C",
                          "P",
                          "time",
-                         "basal_k",
+                         "obs_basal_k",
                          "disturbance",
                          "fcl")
       output[[counter]] <- as.data.frame(out)
